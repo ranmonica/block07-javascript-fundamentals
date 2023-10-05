538053ccf6501beb16333a3bc8c805c1a90de481
Monica Ran <ranmonica96@gmail.com>
2023-10-05 10:52:39 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1. Create a vault that generates three codes in a combination using three mathmetical calculations. 
+
+// 2. Create a string for the user to open the vault with a message.
+
+let str1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+console.log(str1)
+
+// 3. Assign 3 variables using different arithmetic operators.
+
+let num1 = 5 + 5
+let num2 = 10 * 4
+let num3 = 40 - 1
+
+// 4. Create a dialogue box displaying vault numbers.
+
+alert(num1 + "-" + num2 + "-" + num3)
